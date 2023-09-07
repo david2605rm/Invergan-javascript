8391498a03964e427dbff52d65cf2aa19c4a66f0
David Velez Ramirez <davidvelez2605@gmail.com>
2023-09-07 10:51:45 -0500

    preentrega 2

---
+++ inventario.js
@@ -0,0 +1,109 @@
+const inventario = [
+    {
+        raza: "Velazques",
+            caracteristicas:{
+                peso: "720 kg",
+                precio: "1533 usd",
+                ventaDirecta: false,
+                porcentajeRetorno: "8% anual",
+                numero: "233",
+                genero: "hembra",
+            }, 
+        
+    },
+    {
+        raza: "Velazques",
+        caracteristicas:{
+            peso: "860 kg",
+            precio: "1740 usd",
+            ventaDirecta: true,
+            numero: "236",
+            genero: "macho",    
+        },          
+        
+    },
+    {
+        raza: "Velazques",
+        caracteristicas:{
+            peso: "700 kg",
+            precio: "1533 usd",
+            ventaDirecta: false,
+            porcentajeRetorno: "8% anual",
+            numero: "233",
+            genero: "hembra",    
+        },          
+        
+    },
+    {
+        raza: "brahman rojo",
+        caracteristicas:{
+            peso: "620 kg",
+            precio: "1425 usd",
+            ventaDirecta: false,
+            porcentajeRetorno: "7% anual",
+            numero: "155",
+            genero: "hembra",   
+        },          
+        
+    },
+    {
+        raza: "brahman rojo",
+        caracteristicas:{
+            peso: "735 kg",
+            precio: "1570 usd",
+            ventaDirecta: true,
+            numero: "138",
+            genero: "macho",    
+        },          
+        
+    },
+    {
+        raza: "brahman rojo",
+        caracteristicas:{
+            peso: "600 kg",
+            precio: "1480 usd",
+            ventaDirecta: false,
+            porcentajeRetorno: "7% anual",
+            numero: "133",
+            genero: "hembra",   
+        },          
+        
+    },
+    {
+        raza: "Guzera",
+        caracteristicas:{
+            peso: "670 kg",
+            precio: "1690 usd",
+            ventaDirecta: false,
+            porcentajeRetorno: "9% anual",
+            numero: "378",
+            genero: "hembra",    
+        },          
+        
+    },
+    {
+        raza: "Guzera",
+        caracteristicas:{
+            peso: "900 kg",
+            precio: "1930 usd",
+            ventaDirecta: true,
+            numero: "389",
+            genero: "macho",    
+        },          
+        
+    },
+    {
+        raza: "Guzera",
+        caracteristicas:{
+            peso: "621 kg",
+            precio: "1640 usd",
+            ventaDirecta: false,
+            porcentajeRetorno: "9% anual",
+            numero: "374",
+            genero: "hembra",    
+        },          
+        
+    },
+
+]
+
